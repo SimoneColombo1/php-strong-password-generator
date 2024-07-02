968ebce17cec420f76d33534ba1aa679a2bf0f7d
SimoneColombo1 <simcolombo2@gmail.com>
2024-07-02 17:43:48 +0200

    Creato l'array per l'accesso

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+$Login = [
+    [
+        "username" => "gino",
+        "password" => "albertos001",
+
+
+    ],
+
+    [
+        "username" => "stoppo",
+        "password" => "bromvo",
+
+
+    ],
+
+    [
+        "username" => "pino",
+        "password" => "bebus",
+
+
+    ],
+
+    [
+        "username" => "ginozzo",
+        "password" => "albertozzodiddio",
+
+
+    ],
+
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
